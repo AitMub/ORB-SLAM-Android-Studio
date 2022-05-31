c605f045a082a281ffd607c8b4d49b86b542c738
AitMub <89588827+AitMub@users.noreply.github.com>
2022-05-31 20:31:03 +0800

    Add camera zoom

--- app/src/main/cpp/basic_viewer/render_parameter.h
+++ app/src/main/cpp/basic_viewer/render_parameter.h
@@ -46,6 +46,7 @@ struct RenderParameter
 
     // camera
     glm::vec3 cameraPos = glm::vec3(0.0f, 5.0f, 5.0f);
+    glm::vec3 cameraLookAt = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 cameraFront = glm::vec3(0.0f, -1.0f, -1.0f);
     glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
     const float zNear = 0.1f;
